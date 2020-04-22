80a17f90ecfcf8323398460aeb718ba2a249d85f
Kulkejszyn <45689169+Kulkejszyn@users.noreply.github.com>
2020-04-22 10:28:20 +0200

    Update MainActivity

--- app/src/main/java/com/pzm/pizzera/MainActivity.java
+++ app/src/main/java/com/pzm/pizzera/MainActivity.java
@@ -3,17 +3,11 @@ package com.pzm.pizzera;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.pzm.pizzera.register.RegisterActivity;
-
-
 public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		Intent myIntent = new Intent(this, RegisterActivity.class);
-		startActivity(myIntent);
 	}
 }
